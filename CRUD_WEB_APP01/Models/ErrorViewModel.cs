ec59c2e299997afd6fd498a336453c31cb737408
Miki_806 <milan.mm.minic@gmail.com>
2022-04-02 21:14:31 +0200

    Prvi KOMIT

---
+++ CRUD_WEB_APP01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUD_WEB_APP01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
